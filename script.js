860fccaa17dd82ceacb3852d0322ad16f0af0a8c
Mauricio <mjunior447@gmail.com>
2021-05-30 21:46:15 -0300

    criação de arquivo Javascript para atribuir classes CSS dinamicamente a elementos do menu; criação do efeito de transição suave do menu; animação do ícone de sanduíche

---
+++ script.js
@@ -0,0 +1,6 @@
+var sanduiche = document.querySelector(".sanduiche");
+var container = document.querySelector(".container");
+
+sanduiche.addEventListener("click", () => {
+    container.classList.toggle("show-menu");
+})
